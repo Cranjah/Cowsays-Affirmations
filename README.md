af3d8fef2f540c8b377eb55c9e841a2e49f051d3
Marc-Michel Münch <87249874+Cranjah@users.noreply.github.com>
2024-08-24 20:31:11 +0200

    creation of the repository

---
+++ README.md
@@ -0,0 +1,12 @@
+## COWSAYS AFFIRMATIONS AUTOMAT
+### Scientific Background: [Click here to get forwarded to ACM research article](https://dl.acm.org/doi/10.1145/3341525.3387372)
+
+### Description of the Functionalities:
+
+Based on the idea of consulting some aspects of positive psychology to ensure my good performance in the upcoming study period, I developed this little playful program based on the “cowsay” Python library and ultimately on affirmations and the findings of a study on the effects of positive psychology on the study of computer science (see link for the scientific background).
+
+The program simulates the functioning of a snack vending machine, at the beginning you are asked to select a number between 1 and 20 - afterwards you have to pay the total price of 1.20 € or ingame 120 cents in coins. To pay the price, you are asked to enter a common European coin format (the gradations are: 1, 2, 5, 10, 20, 50, 100, 200, which corresponds to coins worth the corresponding amount of cents).
+
+Once you have paid the price for the affirmation, the affirmation selected at the beginning is displayed in the form of an ASCII artwork - based on the “cowsay” Python library. The project was just a small coding project of mine - but I am personally fascinated by the algorithmic approach of vending machines in general and I was very proud when I had programmed my first own automat.
+
+**Sometimes it's the little things that make the difference!**
---
+++ affirmations/project.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import sys
+import cowsay
+import random
+
+def main():
+    amount = 120
+
+    affirmations = ["I learn a little bit every day.",
+                    "I am doing my best.",
+                    "I know more today than I knew yesterday.",
+                    "I am ready to learn something new today.",
+                    "I can ask for help.",
+                    "I can help others who need me.",
+                    "I can achieve my goals.",
+                    "I can do difficult things.",
+                    "I can learn from my mistakes.",
+                    "I can use more than one language to communicate.",
+                    "I play an important role in my community.",
+                    "I accept all of my emotions.",
+                    "I am ready for a new challenge.",
+                    "I am different and unique.",
+                    "I treat others with kindness and respect.",
+                    "I am grateful for my friends and family.",
+                    "I am thankful for my teacher.",
+                    "I take care of my body and mind.",
+                    "I focus on things that matter to me.",
+                    "I feel wonderful."]
+
+    while True:
+        try:
+            choice = int(input("Choose an item between 1 and 20 you want to buy: "))
+
+            if choice > 20 or choice < 1:
+                raise ValueError
+
+        except ValueError:
+            choice = random.randint(1, 20)
+
+        except EOFError:
+            print("")
+            cowsay.cow("You did exit this program on purpose! See you!")
+            sys.exit(1)
+
+        choicecalc = choice - 1
+
+        while amount > 0:
+            try:
+                print(f"Amount due to buy affirmation: {amount} Cents")
+                coin = int(input("Please insert one virtual coin: "))
+
+                if coin == 1:
+                    amount -= 1
+
+                elif coin == 2:
+                    amount -= 2
+
+                elif coin == 5:
+                    amount -= 5
+
+                elif coin == 10:
+                    amount -= 10
+
+                elif coin == 20:
+                    amount -= 20
+
+                elif coin == 50:
+                    amount -= 50
+
+                elif coin == 100:
+                    amount -= 100
+
+                elif coin == 200:
+                    amount -= 200
+
+            except ValueError:
+                continue
+
+            except EOFError:
+                print("")
+                cowsay.cow("I'm sorry to hear, that you ran out of money!")
+                sys.exit(1)
+
+        if amount == 0:
+            print("Thank you very much for your purchase!!!")
+            cowsay.cow(f"{affirmations[choicecalc]}")
+            sys.exit(1)
+
+        elif amount < 0:
+            print(f"We owe you some change for your purchase: {amount} Cents")
+            cowsay.cow(f"{affirmations[choicecalc]}")
+            sys.exit(1)
+
+if __name__ == "__main__":
+    main()
